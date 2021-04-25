e243c198cb0550e56f55f0194b38d4691bea5ad0
Ggicci <ggicci.t@gmail.com>
2021-04-25 23:58:51 +0800

    feat: batch process my files

--- pearson.py
+++ pearson.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 
 import logging
+import math
+import os
 from multiprocessing import Manager, Pool
 from typing import List
 
@@ -32,8 +34,7 @@ def compute_pearson_corr(df: pd.DataFrame):
             df.at[r, c] = shared.left_data.loc[r].corr(shared.right_data.loc[c])
 
 
-def process_cell_group(cell_group: str, genes: List[str]):
-    """Process a cell group."""
+def process_cell_group_parallel(cell_group: str, genes: List[str]):
     gene_data = pd.read_csv(f"./data-20210418/{cell_group}.csv", index_col=0)
     left_set = set(gene_data.index.tolist())
     right_set = set(genes)
@@ -49,15 +50,46 @@ def process_cell_group(cell_group: str, genes: List[str]):
     ans.to_csv(f"./{cell_group}.pearson.all.csv")
 
 
+def process_cell_group(cell_group: str, genes: List[str]):
+    LOGGER.info("process cell group, cell_group=%s, genes=%s", cell_group, genes)
+
+    output_file = f"./{cell_group}.pearson.all.csv"
+    if os.path.exists(output_file):
+        LOGGER.info("skip cus output file exists, file=%s", output_file)
+        return
+
+    input_file = f"./data-20210418/{cell_group}.csv"
+    LOGGER.info("read cell group gene data, file=%s", input_file)
+    gene_data = pd.read_csv(input_file, index_col=0)
+    left_set = set(gene_data.index.tolist())
+    right_set = set(genes)
+    left_data = gene_data.drop(index=[x for x in gene_data.index if x not in left_set])
+    right_data = gene_data.drop(
+        index=[x for x in gene_data.index if x not in right_set]
+    )
+    ans = pd.DataFrame(0, index=left_data.index, columns=right_data.index, dtype=float)
+    LOGGER.info("compute pearson corr, shape=%s", ans.shape)
+    total = ans.size
+    count = 0
+    milestones = [math.ceil(x * total / 100) for x in range(1, 101)]
+    next_milestone = 0
+    for r in ans.index:
+        for c in ans.columns:
+            ans.at[r, c] = left_data.loc[r].corr(right_data.loc[c])
+            count += 1
+            if count == milestones[next_milestone]:
+                LOGGER.info("in progress, percentage=%d%%", next_milestone + 1)
+                next_milestone += 1
+
+    ans.to_csv(output_file)
+
+
 def main():
     meta = pd.read_csv("./data-20210422/Correlated Genes.csv")
     for cell_group in meta.columns:
-        if cell_group != "Myeloid":
-            continue
         gene_series = meta[[cell_group]]
         gene_series = gene_series[gene_series[cell_group].notnull()]
         genes = gene_series[cell_group].tolist()
-        LOGGER.info("process cell group, cell_group=%s, genes=%s", cell_group, genes)
         process_cell_group(cell_group, genes)
 
 
